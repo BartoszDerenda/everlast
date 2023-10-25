61aeacd4f832f7acfa1bad317a785d507f7921d0
BartoszDerenda <bartoszderenda00@gmail.com>
2023-10-25 21:29:11 +0200

    Further update for comment section
    Still working on it. Consider this an evening checkpoint.

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -20,30 +20,32 @@ def profile(request, profile_id):
     profile_warband = Dwarf.objects.values('name', 'battle_power', 'battles_fought').filter(leader=profile_id)
     profile_info = Account.objects.values('username', 'avatar', 'level',
                                           'profile_text', 'gold', 'rubies',
-                                          'last_visited').filter(id=profile_id)
+                                          'reputation', 'last_visited').filter(id=profile_id)
     total_battle_power = 0
     for dwarf in profile_warband:
         total_battle_power += dwarf['battle_power']
 
-    # For some damn reason it throws a tantrum if it's in the 'else' of the 'if request.method == POST'
     create_comment = CreateComment()
+    author = request.user
+    receiver = Account.objects.get(id=profile_id)
 
     if request.method == 'POST':
-        author = request.user
-        receiver = Account.objects.get(id=profile_id)
         if 'create_comment' in request.POST:
             create_comment = CreateComment(request.POST)
             if create_comment.is_valid():
                 text = create_comment.cleaned_data['text']
                 points = create_comment.cleaned_data['points']
-                Account.objects.update(reputation=F('reputation') + points)
+                Account.objects.filter(id=receiver.id).update(reputation=F('reputation') + points)
                 comment = Comment.objects.create(author=author, receiver=receiver, text=text, points=points)
                 comment.save()
         if 'delete_comment' in request.POST:
+            points = Comment.objects.values('points').filter(author=author, receiver=receiver)
+            Account.objects.filter(id=receiver.id).update(reputation=F('reputation') - points)
             Comment.objects.filter(author=author, receiver=receiver).delete()
 
     # After POST so that the comment is visible on first refresh.
     comments = Comment.objects.all().filter(receiver=profile_id)
+    your_comment = Comment.objects.all().filter(receiver=receiver, author=request.user)
 
     # Add redirect with context (impossible?) when user tries to search with URL for a user with ID that does not exist.
 
@@ -53,5 +55,6 @@ def profile(request, profile_id):
         'profile_warband': profile_warband,
         'total_battle_power': total_battle_power,
         'comments': comments,
-        'create_comment': create_comment
+        'create_comment': create_comment,
+        'your_comment': your_comment
     })
